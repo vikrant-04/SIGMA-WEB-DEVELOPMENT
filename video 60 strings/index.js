2aa52777c987b67ef3f8c6ef6584344c42309063
vikrant-04 <vikrantyadav4802@gmail.com>
2023-11-26 10:25:18 +0530

    c

---
+++ video 60 strings/index.js
@@ -0,0 +1,10 @@
+console.log("this is a string tutorial")
+
+let a ="harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[5]);  // we will show undefined
+
+//templet literals
